e924b6996f6ba5345a501558ad6e1a9607f9eca7
ongaeshi <ongaeshi0621@gmail.com>
2024-12-08 01:07:41 +0900

    コンポーネントテストを追加

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
             items: [
                 { text: 'Index', link: '/test/' },
                 { text: 'Simple Counter', link: '/test/simple_counter' },
+                { text: 'コンポーネントテスト', link: '/test/component_test' },
                 { text: 'lodash を使って文字列をシャッフル', link: '/test/shuffle_string_with_lodash' },
-                { text: '文字数カウンター', link: '/test/character_counter' }
             ]
         }
         ],
---
+++ docs/test/component_test.md
@@ -0,0 +1,6 @@
+<script setup>
+import ButtonCounter from '../components/ButtonCounter.vue'
+</script>
+
+<h1>Here is a child component!</h1>
+<ButtonCounter />
